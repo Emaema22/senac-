6a649e15edee5c3e0d219feb683d6fddc2885ccc
Emaema22 <emanuelaferreiradealmeida10@gmail.com>
2025-07-17 08:29:19 -0300

    Add files via upload

---
+++ Comparacao de numeros 2.py
@@ -0,0 +1,14 @@
+print("===Comparar dois números==")
+ # Ler dois númeroa da tela
+a = int(input("Digite um número:"))
+b = int(input("Digite outro número:"))
+#Comparar os dois números
+if( a == b):
+    print("Números iguais!")
+else:
+    print("Números diferentes!")
+ #Mostrar o número maior
+if( a > b):
+    print("O maior é a = ",a)
+else:
+    print("O maior é b = ", b)
